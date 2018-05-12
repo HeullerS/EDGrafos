030094512a9ecd0167ca747928389c3f5c150dc6
willdc4 <williamdcoelho@gmail.com>
2018-05-11 23:00:52 -0300

    Grafo adicionado

---
+++ Grafo.py
@@ -0,0 +1,8 @@
+#Classe que representa um grafo lido do arquivo
+class Grafo(object):
+	def __init__(self,listaVertices, listaArestas, ehDirecionado, ehPonderado):
+		self.vertices = listaVertices
+		listaArestas.sort()
+		self.arestas = listaArestas
+		self.direcionado = ehDirecionado
+		self.ponderado = ehPonderado
